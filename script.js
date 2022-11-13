69893f19b878d3402f8137efaed643ed5d7bb9eb
Konrad <konradbieniek@gmail.com>
2022-11-13 22:37:06 +0100

    event listener on button

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ const btn = document.getElementById("button")
 
 const selectMediaStream = async () => {
 	try {
-		dupa
+		const media = await navigator.mediaDevices.getDisplayMedia()
+		videoElement.srcObject = media
+		videoElement.onloadedmetadata = () => {
+			videoElement.play()
+		}
 	} catch (err) {
 		// catch err
 		console.log("error:", err)
 	}
 }
+
+btn.addEventListener("click", async () => {
+	btn.disabled = true
+
+	await videoElement.requestPictureInPicture()
+
+	btn.disabled = false
+})
 
 selectMediaStream()
