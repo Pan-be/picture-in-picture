2b0ce240a7d041058f0210931a55bbd948eaf89d
Konrad <konradbieniek@gmail.com>
2022-11-13 20:57:39 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const videoElement = document.getElementById("video")
+const btn = document.getElementById("button")
+
+const selectMediaStream = async () => {
+	try {
+		dupa
+	} catch (err) {
+		// catch err
+		console.log("error:", err)
+	}
+}
+
+selectMediaStream()
